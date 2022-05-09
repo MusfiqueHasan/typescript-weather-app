b6ae21f1be10f6c05553469ea8b4ed98db0fdcc6
Musfique Hasan <musfique.hasan.professional@gmail.com>
2022-05-09 16:26:20 +0600

    some updates are done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CircularProgress } from '@mui/material';
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NotFound from './components/pages/NotFound';
 
 const Home = lazy(() => import('./components/pages/Home'));
 const CountryInfo = lazy(() => import('./components/pages/CountryInfo'));
@@ -14,6 +15,7 @@ const App:React.FC = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/country/:name" element={<CountryInfo />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </Suspense>
